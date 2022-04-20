2a871a00bb0b11e60eeaafd45071883c370a9fc8
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-04-20 15:29:29 +0200

    feat: Update DropdownButton
    BREAKING CHANGE: We add `textVariant`, `spaceBetween` and `disabled` props to
    be able to change the size of the icon, and disabled the component.
    Besides it's no longer needed to use Typography as first child of
    DropdownButton since it's wrapped inside, but the old school way
    still works as before.
    It's still possible to use the Dropdown without the button wrapper, as before, use `Dropdown` component instead of `DropdownButton`.

--- react/DropdownButton/Readme.md
+++ react/DropdownButton/Readme.md
@@ -1,29 +1,52 @@
 This component can be used as a trigger to open menus, for example an ActionMenu component.
 
 ```jsx
-import DropdownButton from 'cozy-ui/transpiled/react/DropdownButton';
-import Typography from "cozy-ui/transpiled/react/Typography";
+import DropdownButton from 'cozy-ui/transpiled/react/DropdownButton'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+import Grid from 'cozy-ui/transpiled/react/MuiCozyTheme/Grid'
 
-<div>
-  <div>
-    <DropdownButton>
-      <Typography variant="h3" component="h1">Cozy</Typography>
+const variants = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'subtitle1',
+  'subtitle2',
+  'body1',
+  'body2',
+  'caption'
+]
+
+;
+
+<>
+  <Grid container>
+    <Grid item xs={6}>
+      Default
+      {variants.map((variant, index) => (
+        <div key={index} className='u-mb-1'>
+          <DropdownButton textVariant={variant}>
+            {variant}
+          </DropdownButton>
+        </div>
+      ))}
+    </Grid>
+    <Grid item xs={6}>
+      Disabled
+      {variants.map((variant, index) => (
+        <div key={index} className='u-mb-1'>
+          <DropdownButton textVariant={variant} disabled>
+            {variant}
+          </DropdownButton>
+        </div>
+      ))}
+    </Grid>
+  </Grid>
+  <div style={{ border: '1px solid var(--borderMainColor)', width: '100%' }}>
+    <DropdownButton fullWidth spaceBetween>
+      Full width and space between text and icon
     </DropdownButton>
   </div>
-  <div>
-    <DropdownButton>
-      <Typography variant="h4">Cozy</Typography>
-    </DropdownButton>
-  </div>
-  <div>
-    <DropdownButton>
-      <Typography variant="h5">Cozy</Typography>
-    </DropdownButton>
-  </div>
-  <div>
-    <DropdownButton>
-      <Typography variant="body1">Cozy</Typography>
-    </DropdownButton>
-  </div>
-</div>
+</>
 ```
--- react/DropdownButton/index.jsx
+++ react/DropdownButton/index.jsx
@@ -1,26 +1,57 @@
-import React from 'react'
-import cx from 'classnames'
-import styles from './styles.styl'
-import Icon from '../Icon'
+import React, { forwardRef } from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
 
-import BottomIcon from 'cozy-ui/transpiled/react/Icons/Bottom'
+import Dropdown from '../Dropdown'
 
-const DropdownButton = React.forwardRef(
-  ({ children, className, ...props }, ref) => (
-    <button
-      role="button"
-      className={cx(styles['c-DropdownButton'], className)}
-      ref={ref}
-      {...props}
-    >
-      {children}
-      <Icon
-        icon={BottomIcon}
-        size="12"
-        className={styles['c-DropdownButton-Icon']}
-      />
-    </button>
-  )
+const useStyles = makeStyles({
+  root: {
+    height: 'auto',
+    width: ({ fullWidth }) => (fullWidth ? `calc(100% + 16px)` : 'auto'),
+    margin: '-8px',
+    padding: '8px'
+  },
+  text: {
+    textTransform: 'none'
+  }
+})
+
+const DropdownButton = forwardRef(
+  (
+    {
+      spaceBetween = false,
+      textVariant = 'body1',
+      disabled = false,
+      fullWidth = false,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    const { root, text } = useStyles({ fullWidth })
+
+    return (
+      <Button ref={ref} classes={{ root, text }} disabled={disabled} {...props}>
+        <Dropdown
+          variant={textVariant}
+          spaceBetween={spaceBetween}
+          disabled={disabled}
+        >
+          {children}
+        </Dropdown>
+      </Button>
+    )
+  }
 )
+
+DropdownButton.propTypes = {
+  /** Weither there is a space between the label and the icon */
+  spaceBetween: PropTypes.bool,
+  /** Like variant from Typography component */
+  textVariant: PropTypes.string,
+  disabled: PropTypes.bool,
+  fullWidth: PropTypes.bool
+}
 
 export default DropdownButton
